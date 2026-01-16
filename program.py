b6758be8509f669d8c598b71ae44300b2af71280
Priyank <kadianpriyank19@gmail.com>
2026-01-16 14:42:10 +0530

    first-commit

---
+++ program.py
@@ -0,0 +1,9 @@
+import os
+import torch
+import cv2
+from torch.utils.data import Dataset
+
+class MVTechDataset(Dataset):
+    def __init__(self,root,split="train",img_size=224):
+        
+        return
